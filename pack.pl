36454d42bc22cd47d8ddfb3cafa8c6ed717bfa7a
root <logicmoo@gmail.com>
2017-03-08 17:32:37 -0800

    v1.1.111

--- pack.pl
+++ pack.pl
@@ -1,9 +1,10 @@
 name(with_thread_local).
 title('Call a Goal with local assertions').
-version('0.0.8').
+version('1.1.111').
 download('https://github.com/TeamSPoon/with_thread_local/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home('https://github.com/TeamSPoon/with_thread_local').
+requires(each_call_cleanup).
 autoload(true).
