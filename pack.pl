bbd79be97d825166be4bca426598594a7cf0a8ff
Douglas R. Miles <logicmoo@gmail.com>
2019-01-20 23:39:03 +0000

    All files have been moved into logicmoo_utils

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(with_thread_local).
 title('Call a Goal with local assertions').
-version('1.1.118').
+version('1.2.111').
 download('https://github.com/TeamSPoon/with_thread_local/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home('https://github.com/TeamSPoon/with_thread_local').
-requires(each_call_cleanup).
+requires(logicmoo_utils).
 autoload(true).
